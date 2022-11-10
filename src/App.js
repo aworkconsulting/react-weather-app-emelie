a9e382b7d848a3564063802b2fcbfd05b6ca8ab1
aworkconsulting <emelie@aworkconsulting.se>
2022-11-10 16:40:27 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {" "}
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a href="https://aworkconsulting.se/" target="_blank" rel="noreferrer">
-          Emelie Jansson{" "}
-        </a>
-        and is{" "}
-        <a
-          href="https://github.com/aworkconsulting/react-weather-app-emelie"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="coontainer">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://aworkconsulting.se/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Emelie Jansson{" "}
+          </a>
+          and is{" "}
+          <a
+            href="https://github.com/aworkconsulting/react-weather-app-emelie"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
