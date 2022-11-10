7c040608dee2a7702824aba7f7ec59ae226e2e22
aworkconsulting <emelie@aworkconsulting.se>
2022-11-10 16:23:16 +0100

    added the github url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      Weather App
+      <footer>
+        This project is coded by{" "}
+        <a href="https://aworkconsulting.se/" target="_blank">
+          Emelie Jansson{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/aworkconsulting/react-weather-app-emelie"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
