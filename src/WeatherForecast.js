51d6634b09bd465e53d3d0e27c39b20832118f83
aworkconsulting <emelie@aworkconsulting.se>
2022-11-15 17:24:04 +0100

    added long and lat

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(repsone) {}
+
+  let apiKey = "ca2c5297af3e10f6430a712560f08ff1";
+  let longitude = props.coordinates.long;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
